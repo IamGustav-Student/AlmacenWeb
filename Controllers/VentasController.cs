1644a5cabdac8c18e29846efe2c10890e399572c
IamGustav-Student <iamgustav.olivera@gmail.com>
2025-09-29 17:35:24 -0300

    Add files via upload

---
+++ Controllers/VentasController.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AlmacenWeb.Data;
+using AlmacenWeb.Models;
+
+namespace AlmacenWeb.Controllers
+{
+    public class VentasController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public VentasController(AppDbContext context)
+        {
+            _context = context;
+        }
+        // Acción para buscar producto por código de barras (usando AJAX)
+        [HttpPost]
+        public async Task<IActionResult> BuscarProducto([FromBody] string codigoBarra)
+        {
+            var producto = await _context.Productos.FirstOrDefaultAsync(p => p.CodigoBarra == codigoBarra);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            return Json(producto);
+        }
+
+        // GET: Ventas/PuntoVenta
+        public IActionResult PuntoVenta()
+        {
+            return View();
+        }
+
+        // GET: Ventas
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Ventas.Include(v => v.Cliente);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Ventas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var venta = await _context.Ventas
+                .Include(v => v.Cliente)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (venta == null)
+            {
+                return NotFound();
+            }
+
+            return View(venta);
+        }
+
+        // GET: Ventas/Create
+        public IActionResult Create()
+        {
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "ClId", "ClApellido");
+            return View();
+        }
+
+        // POST: Ventas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FechaVenta,Total,ClienteId")] Venta venta)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(venta);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "ClId", "ClApellido", venta.ClienteId);
+            return View(venta);
+        }
+
+        // GET: Ventas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var venta = await _context.Ventas.FindAsync(id);
+            if (venta == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "ClId", "ClApellido", venta.ClienteId);
+            return View(venta);
+        }
+
+        // POST: Ventas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FechaVenta,Total,ClienteId")] Venta venta)
+        {
+            if (id != venta.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(venta);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VentaExists(venta.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "ClId", "ClApellido", venta.ClienteId);
+            return View(venta);
+        }
+
+        // GET: Ventas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var venta = await _context.Ventas
+                .Include(v => v.Cliente)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (venta == null)
+            {
+                return NotFound();
+            }
+
+            return View(venta);
+        }
+
+        // POST: Ventas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var venta = await _context.Ventas.FindAsync(id);
+            if (venta != null)
+            {
+                _context.Ventas.Remove(venta);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VentaExists(int id)
+        {
+            return _context.Ventas.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design.Internal;
+using AlmacenWeb.Models;
+
+namespace AlmacenWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        // Database context implementation
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            // Constructor logic here
+
+        }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
+        public DbSet<DetalleVenta> DetalleVenta { get; set; }
+
+    }
+
+}
