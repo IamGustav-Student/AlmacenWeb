dc4d4fd638803534c4ab1616e50224bb77010348
IamGustav-Student <iamgustav.olivera@gmail.com>
2025-09-29 17:34:51 -0300

    Add files via upload

---
+++ Migrations/20250929000016_v1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlmacenWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class v1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    PrId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PrNombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    CodigoBarra = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Precio = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CantidadDisponible = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.PrId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Productos");
+        }
+    }
+}
---
+++ ViewModels/VentaViewModel.cs
@@ -0,0 +1,10 @@
+﻿using AlmacenWeb.Models;
+
+namespace AlmacenWeb.ViewModels
+{
+    public class VentaViewModel
+    {
+        public ICollection<DetalleVenta> Carrito { get; set; } = new List<DetalleVenta>();
+        public decimal Total { get; set; }
+    }
+}
